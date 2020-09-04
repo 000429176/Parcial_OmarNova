fea2d5a4f8cfa4f734f02da92dc957a3ef0e139a
Omar Freddy Nova Bustamante <omar.nova.2020@upb.edu.co>
2020-09-04 18:03:50 -0500

    Creación del Login

---
+++ Parcial_OmarNova/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parcial_OmarNova
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text=="Admin" && txtPassword.Text=="1234")
+            {
+
+            }
+            else if (txtUsername.Text == "Trabajador" && txtPassword.Text == "5678")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
+        }
+    }
+}
